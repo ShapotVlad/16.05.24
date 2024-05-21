c751ec35bf10e7ee1f195326c5795933be93f126
Vladimir <vlnshapat@gmail.com>
2024-05-21 18:55:27 +0300

    add homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿//разворот строки  наоборот
+
+
+string strNew = "Hello my friend "; //создание строки
+
+string[] strNewSplit = strNew.Split(); //["Hello" "my" "friend"]  создание массива  для метода  разворота Split
+
+string res = "";                                  //создаем новую строку  для размещение разворотной строки
+for (int i = strNewSplit.Length - 1; i >= 0; i--) //цикл разворота
+{
+    res += strNewSplit[i] + " ";      // записываем  в новую  строку слова, разворот по пробелам
+}
+
+Console.WriteLine(res); // вывод  новой строки
