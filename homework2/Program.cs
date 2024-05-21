63663e479a4860456dfabbfb9906105f52952087
Vladimir <vlnshapat@gmail.com>
2024-05-21 18:12:09 +0300

    добавили пояснения

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -5,15 +5,15 @@
 // 21:46
 // “aBcD1ef!-” => “abcd1ef!-”
 
-string UpperToLower(string str)
+string UpperToLower(string str)                     //создаем метод(функцию) перевода символов введенной строки  в строку с буквами строчного регистра
 {
-    string strResult = string.Empty;
+    string strResult = string.Empty;                //создаем строку для переноса в него символов
 
-    for (int i = 0; i < str.Length; i++)
+    for (int i = 0; i < str.Length; i++)            //цикл  прохода  введенной строки
     {
-        if (char.IsUpper(str[i]))
+        if (char.IsUpper(str[i]))                   // метод определяет  содержит ли строка заглавгые буквы
         {
-            strResult += char.ToLower(str[i]);
+            strResult += char.ToLower(str[i]);      // преобразует заглавные в прописные буквы и формирует строку
         }
         else
         {
