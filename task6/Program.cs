646ce66cdc2ade71ec4a053a8a2b07f6c9e5c0e7
Vladimir <vlnshapat@gmail.com>
2024-05-21 17:43:57 +0300

    добавили пояснения

--- task6/Program.cs
+++ task6/Program.cs
@@ -5,28 +5,28 @@
 // “hello” => 2
 // “world” => 1
 
-int CountVowels(string str)
+int CountVowels(string str) //создаем метод(функцию) для подсчета главсных в веденной строке с консоли
 {
-    string vowels = "aeiouy";
-    int count = 0;
-    for (int i = 0; i < str.Length; i++)
+    string vowels = "aeiouy"; //создали переменную строки гласных
+    int count = 0;            //создали переменную для подсчета гласных 
+    for (int i = 0; i < str.Length; i++) // цикл перебора строки
     {
-        for (int j = 0; j < vowels.Length; j++)
+        for (int j = 0; j < vowels.Length; j++) //цикл  перебора  переменной строки гласной
         {
-            if (str[i] == vowels[j])
+            if (str[i] == vowels[j]) //сравниваем элементы  строки введенной и строки гласных
             {
-                count++;
+                count++;  //если в строке есть гласные увеличиваем счетчик
             }
         }
     }
-    return count;
+    return count; //возврат переменной счетчика в функцию
 }
 
-Console.WriteLine("Напишите слово: ");
-string start = Console.ReadLine();
-Console.WriteLine($"В слове {CountVowels(start)} гласных");
+Console.WriteLine("Напишите слово: "); //ввод слова
+string start = Console.ReadLine(); // ввод
+Console.WriteLine($"В слове {CountVowels(start)} гласных");//вызов функции CountVowels(string str) и вывод счетчика
 
-Console.WriteLine();
+Console.WriteLine();//разрыв строки
 
 //второй метод
 
@@ -36,7 +36,7 @@ int QuantityVowels(string str)
     int count = 0;
     for (int i = 0; i < str.Length; i++)
     {
-        if (vowels.Contains(str[i]))
+        if (vowels.Contains(str[i])) //подсчет гласных методом  Contains
         {
             count++;
         }
