227345a66411dbf5855fd7bb30339addbfd8defc
Vladimir <vlnshapat@gmail.com>
2024-05-23 10:33:45 +0300

    add homework1

---
+++ homework1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+    
+        // Создаем двумерный массив символов
+        char[,] charArray = new char[,]
+        {
+            { 'H', 'e', 'l', 'l', 'o' },
+            { 'W', 'o', 'r', 'l', 'd' }
+        };
+
+        // Создаем строку из массива символов
+        string result = new string(GetCharArrayAs1D(charArray));
+
+        // Выводим результат
+        Console.WriteLine("Результирующая строка: " + result);
+    
+
+    // Метод для преобразования двумерного массива символов в одномерный
+    static char[] GetCharArrayAs1D(char[,] charArray)
+    {
+        int rows = charArray.GetLength(0);
+        int cols = charArray.GetLength(1);
+        char[] result = new char[rows * cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i * cols + j] = charArray[i, j];
+            }
+        }
+
+        return result;
+    }
+
