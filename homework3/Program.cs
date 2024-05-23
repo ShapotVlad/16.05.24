a50dc5abc8e5aba51f98adee6e6921622eba49e3
Vladimir <vlnshapat@gmail.com>
2024-05-23 11:01:59 +0300

    add homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Строка \"{0}\" является палиндромом.", input);
+        }
+        else
+        {
+            Console.WriteLine("Строка \"{0}\" не является палиндромом.", input);
+        }
+    
+
+   static bool IsPalindrome(string str)
+    {
+        // Удаляем пробелы и приводим к нижнему регистру
+        str = str.Replace(" ", "").ToLower();
+
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            if (str[i] != str[str.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
